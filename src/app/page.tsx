136251d063d928298fd43097e7c62a9af6c05818
_shiina <115807888+Drymartini2199@users.noreply.github.com>
2025-01-20 12:38:28 +0700

    tttt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Navbar from '@/components/Navbar';
+<<<<<<< Updated upstream
 import Header from '@/components/Header';
+=======
+import Footer from '@/components/Footer';
+>>>>>>> Stashed changes
 
 function Page() {
   return (
     <div>
+<<<<<<< Updated upstream
       <Header/>
       <Navbar/>
+=======
+      <Navbar />  {/* เรียกใช้ Navbar ในหน้าของคุณ */}
+      <div>Page Content</div>
+      <Footer />  {/* เรียกใช้ Footer ในหน้าของคุณ */}
+>>>>>>> Stashed changes
     </div>
   );
 }
