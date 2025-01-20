97cea908a093e145654ed3926314d90ffb9a869e
Rangtude <panggylovely@gmail.com>
2025-01-20 16:30:40 +0700

    navbar component

--- src/app/arttoy/page.tsx
+++ src/app/arttoy/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '@/components/Navbar';
 import Header from '@/components/Header';
 
-function Toymatch() {
+function Arttoy() {
   return (
     <div>
       <Header/>
@@ -11,4 +11,4 @@ function Toymatch() {
   );
 }
 
-export default Toymatch;
+export default Arttoy;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
-import '../Styles/Navbar.scss'; 
+"use client";
+import React, { useState } from "react";
+import "../Styles/Navbar.scss";
+import Link from "next/link";
+
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="navbar">
-      <a href="/" className="logo">MyLogo</a>
-      <ul className="nav-links">
-        <li><a href="/">Toy Match</a></li>
-        <li><a href="/about">Art Toy</a></li>
-        <li><a href="/contact">Community</a></li>
-      </ul>
+      <div className="container">
+        <img src="/Images/Logo.png" alt="Logo" className="Logo" />
+      </div>
+
+      <div className="nav-links">
+        <a href="/toymatch">Toy Match</a>
+        <a href="/arttoy">Art Toy</a>
+        <a href="/community">Community</a>
+      </div>
+
+      <div className="search-container">
+        <div className="search-wrapper">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search for art toys"
+          />
+          <img
+            src="/Images/Search.png"
+            alt="Search Icon"
+            className="search-icon"
+          />
+        </div>
+      </div>
+
+      <div className="container">
+        <img src="/Images/Favorite.png" alt="Favorite" className="Favorite" />
+        <img src="/Images/Cart.png" alt="Cart" className="Cart" />
+        <Link href="/login">
+          <p className="gradient-button">Login</p>
+        </Link>
+      </div>
     </div>
+
+    
   );
-}
+};
 
 export default Navbar;
